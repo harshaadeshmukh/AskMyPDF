45e783ffe7a2ca054a528d2e6ff49a3eb721cafe
Harshad Deshmukh <147603991+harshaadeshmukh@users.noreply.github.com>
2025-09-16 17:58:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,20 @@
-# GOOGLE_API_KEY = "AIzaSyB3mH0C4fMkZWGCzU5FBca9mwS8_B0dBTk"
-# SUPABASE_URL = "https://nqnpczydxmzchriqiftp.supabase.co"
-# SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xbnBjenlkeG16Y2hyaXFpZnRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5MzM4NTksImV4cCI6MjA3MzUwOTg1OX0.HPlbVDAFtM1gWp1_ptqawY9j4Rqj4ddcrakKlnBq4eA"
-
 import streamlit as st
 
 GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+# how to add supabase details here
+# first go to https://supabase.com/ and create a free account
+# then create a new project and get the details from project overview page
+# and after that add your API key and URL here
+# SUPABASE_URL = "your_supabase_url"
+# SUPABASE_KEY = "your_anon_key"
+
+
+# how to add google api key
+# first go to https://console.cloud.google.com/ and create a free account
+# after that create a new project and enable the Custom Search API
+# then create credentials and get the API key
+# GOOGLE_API_KEY = "your_google_api_key"
+
